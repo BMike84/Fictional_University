29b1ec19f803f18219095308ebed5f5e3cf51bc6
Michael Buffone <m_buffone@hotmail.com>
2023-08-28 13:04:27 -0400

    added past events page

--- wp-content/themes/fictional-university-theme/page-past-events.php
+++ wp-content/themes/fictional-university-theme/page-past-events.php
@@ -6,10 +6,10 @@
   <div class="page-banner__content container container--narrow">
     <!-- This is to make dymanic title for page if category or author using wordpress function -->
     <h1 class="page-banner__title">
-      All Events
+      Past Events
     </h1>
     <div class="page-banner__intro">
-      <p>See what is going on in our world.</p>
+      <p>A recap of our past events.</p>
     </div>
   </div>
 </div>
@@ -17,8 +17,26 @@
 <!-- for event card sections -->
 <div class="container container--narrow page-section">
   <?php 
-    while(have_posts()) {
-      the_post(); 
+  //find only old events
+      $today = date('Ymd');
+        $pastEvents = new WP_Query(array(
+          'paged' => get_query_var('paged', '1'),
+          'post_type' => 'event',
+          'meta_key' => 'event_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'event_date',
+              'compare' => '<',
+              'value' => $today,
+              'type' => 'numeric'
+            ),
+          )
+        ));
+
+    while($pastEvents->have_posts()) {
+      $pastEvents->the_post(); 
   ?>
   <div class="event-summary">
     <a class="event-summary__date t-center" href="#">
@@ -40,13 +58,11 @@
   <?php 
     }
     // This adds paginate using a built in wordpress function
-    echo paginate_links();
+    // this makes paginate work with our custom query
+    echo paginate_links(array(
+      'total' => $pastEvents->max_num_pages
+    ));
   ?>
-
-  <hr class="section-break">
-
-  <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events') ?>">Check out our past events
-      archive</a>.</p>
 </div>
 <!-- for event card sections -->
 
